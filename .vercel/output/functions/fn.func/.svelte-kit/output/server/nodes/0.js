42a2c8cc9d117eb79545d6b7a26b25f72cedffb3
Luuk <luukbrauckmann@gmail.com>
2023-11-25 15:38:55 +0100

    Removed build

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.50fb4686.js","_app/immutable/chunks/scheduler.df919faf.js","_app/immutable/chunks/index.fb517cce.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.2c31a8e5.js","_app/immutable/chunks/scheduler.df919faf.js","_app/immutable/chunks/index.fb517cce.js","_app/immutable/chunks/singletons.3f4a8c3b.js"];
-export const stylesheets = [];
-export const fonts = [];
